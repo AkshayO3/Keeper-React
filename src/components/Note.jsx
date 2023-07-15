02db58fd8eb1d90d919db924aff8039800cbf428
AkshayO3 <akshaybhandari020@gmail.com>
2023-07-15 20:05:38 +0530

    Part-2

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,17 @@
 import React from "react"
+import Card from "./Card"
+import notes from "../notes";
+
+
 function Note(){
     return(
-        <div class="note">
-            <h1>This is the note title.</h1>
-            <p>This is the note content.</p>
+        <div>
+            {notes.map(Single=>
+                <Card
+                    key={Single.id}
+                    title={Single.title}
+                    content={Single.content}/>)}
         </div>
-)
+    )
 }
 export default Note
